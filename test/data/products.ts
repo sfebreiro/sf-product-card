045c803d92eb25ac62731233de07eb8c9b11e490
Samuel Febreiro <samuelfebreiro@gmail.com>
2024-01-31 09:45:14 +0100

    v0.0.1 lista

---
+++ test/data/products.ts
@@ -0,0 +1,11 @@
+export const product1 = {
+    id: '1',
+    title: 'Coffe Mug - Sin Imagen',
+    // img: './coffee-mug.png'
+  }
+
+export const product2 = {
+    id: '2',
+    title: 'Coffe Mug - Con Imagen',
+    img: './coffee-mug.png'
+  }
