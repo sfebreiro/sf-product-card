23b3f3e2e95c5079b997e4cb337f2025eaca6f56
Samuel Febreiro <samuelfebreiro@gmail.com>
2024-01-31 08:58:06 +0100

    Primer commit

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,51 @@
+import React, { createContext } from 'react';
+import { useProduct } from '../hooks/useProduct';
+import { Product, ProductContextProps, OnChangeArgs, InitialValues, ProductCardHandlers } from '../interfaces/interfaces';
+import styles from '../styles/styles.module.css';
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export interface Props {
+    product: Product;
+    // children?: ReactElement | ReactElement[];
+    children: (args: ProductCardHandlers) => JSX.Element;
+    className?: string;
+    style?: React.CSSProperties;
+    onChange?: (args: OnChangeArgs) => void;
+    value?: number;
+    initialValues?: InitialValues;
+}
+
+export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props) => {
+
+    const { counter, increaseBy, maxCount, isMaxCountReached, reset } = 
+        useProduct({onChange, product, value, initialValues});
+
+    return (
+        <Provider value={{
+            counter,
+            increaseBy,
+            product,
+            maxCount
+        }} >
+            <div 
+                className={`${styles.productCard} ${className}`}
+                style={style}
+            >
+
+                {
+                children({
+                    count: counter,
+                    isMaxCountReached,
+                    maxCount: initialValues?.maxCount,
+                    product,
+                    increaseBy,
+                    reset,
+                })
+                }
+                
+            </div>
+        </Provider>
+    )
+}
---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from 'react';
+import { Product, OnChangeArgs, InitialValues } from '../interfaces/interfaces';
+
+interface UseProductArgs {
+    product: Product;
+    onChange?: (args: OnChangeArgs) => void;
+    value?: number;
+    initialValues?: InitialValues;
+}
+
+export const useProduct = ({onChange, product, value = 0, initialValues}: UseProductArgs) => {
+
+    const [counter, setCounter] = useState<number>(initialValues?.count || value);
+    const isMounted = useRef(false);
+    
+    const increaseBy = (value: number) => {
+
+        let newValue = Math.max(counter + value, 0);
+        if (initialValues?.maxCount) {
+            newValue = Math.min(newValue, initialValues.maxCount)
+        }
+
+        setCounter(newValue); 
+
+        onChange && onChange({count: newValue, product});
+    }
+
+    const reset = () => {
+        setCounter(initialValues?.count || value)
+    }
+
+    useEffect(() => {
+      isMounted.current = true;
+    }, [])
+    
+    useEffect(() => {
+        if (!isMounted.current) return;
+
+        setCounter(value);
+    }, [value])
+
+    
+    
+    return {
+        counter, 
+        increaseBy,
+        maxCount: initialValues?.maxCount,
+        isMaxCountReached: !!initialValues?.count && initialValues.maxCount === counter,
+        reset
+    }
+    
+}
